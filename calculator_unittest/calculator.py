357a92c45de763ee630e9bb28dac7306e78a8b3a
Sabrina Haßlocher <62216740+Saccha@users.noreply.github.com>
2022-03-29 11:14:13 -0300

    Add files via upload

---
+++ calculator_unittest/calculator.py
@@ -0,0 +1,37 @@
+class Calculadora():
+    """Performs the four basic mathematical operations
+    Methods:
+     add(number, number)
+     subtract(number, number)
+     multiply(number, number)
+     divide(number,number)
+    """
+
+    def add(self, x, y):
+        """Adds two numbers together
+        Arguments:
+          x - Any number
+          y - Any number
+        """
+        return x + y
+    def sub(self, x, y): 
+        """Subtracts two numbers together
+        Arguments:
+          x - Any number
+          y - Any number
+        """
+        return x - y
+    def mult(self, x, y):
+        """Multiplies two numbers together
+        Arguments:
+          x - Any number
+          y - Any number
+        """
+        return x * y
+    def div(self, x, y):   
+        """Divides two numbers together
+        Arguments:
+          x - Any number
+          y - Any number
+        """
+        return x / y
